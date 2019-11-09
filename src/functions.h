1b55859485e6d37590bec18a30dd492f25264754
SamBozman <s.j.bozman@gmail.com>
2019-11-09 04:55:32 -0800

    Removed experimental ConCat function

--- src/functions.h
+++ src/functions.h
@@ -1,26 +1,6 @@
 #pragma once
 #include "declarations.h"
 
-// char* conCat(char* char1, char* char2){
-//     char filename[128];
-//     char* name = "hello";
-//     char* extension = ".txt";
-
-//     if (sizeof(filename) < strlen(name) + 1 ) { /* +1 is for null character */
-//         fprintf(stderr, "Name '%s' is too long\n", name);
-//         return EXIT_FAILURE;
-//     }
-//     strncpy(filename, name, sizeof(filename));
-
-//     if (sizeof(filename) < (strlen(filename) + strlen(extension) + 1) ) {
-//         fprintf(stderr, "Final size of filename is too long!\n");
-//         return EXIT_FAILURE;
-//     }
-//     strncat(filename, extension, (sizeof(filename) - strlen(filename)) );
-//     printf("Filename is %s\n", filename);
-
-//     return EXIT_SUCCESS;
-// }
 //*********************************************************
 
 void dataInCallback(char *topic, byte *payload, unsigned int length)
