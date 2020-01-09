62ec1ca9fd3a81b3c6fc94956439fa5bf771164a
SamBozman <s.j.bozman@gmail.com>
2020-01-09 06:44:21 -0800

    Fixed code in Setup

--- src/setup.h
+++ src/setup.h
@@ -14,7 +14,6 @@ void setup() //Runs once when first starting program (reset)
   Serial.println("Attempting Auto WiFi Connect!)");
   wifiManager.autoConnect("AutoConnectAP");
   //Program will be blocked here until WiFi has been set up.
-  Serial.println("SUCCESS - Auto Connected during setup!)");
 
   //FIXME: This address has been set as static
   const char *mqtt_server = "192.168.1.11";
@@ -25,13 +24,15 @@ void setup() //Runs once when first starting program (reset)
   esp_sleep_enable_ext0_wakeup(GPIO_NUM_13, 0); //WAKE UP CHIP ON LOW
 
   //Increment boot number and print it every reboot
-  Serial.println("Boot number: " + String(bootCount));
   ++bootCount;
+  Serial.println("Boot number: " + String(bootCount));
+  Serial.println("Total accumulated time : " + String(totTime) + " seconds");
+  Serial.println();
+  delay(3000); //delay long enough to read the print lines above
 
-  lastNow = 0; //used by mqttConnect (non-blocking)
-  runTime = 0; // reset runTime
-  startTime = millis();
-  Serial.println("Unit running, Starting Timer. ");
+  lastNow = 0;          //used by mqttConnect (non-blocking)
+  runTime = 0;          // reset runTime
+  startTime = millis(); //Mark the beginning of our accumulated time for hourmeter
 
   // FIXME:   Test that this following code works properly
 
@@ -42,10 +43,5 @@ void setup() //Runs once when first starting program (reset)
     Serial.println(F("UnitID does = 0"));
     strcpy(UnitID, "N/A"); //Change UnitID to N/A until config file is located
     saveConfig();          //Save temporary default configuration with UnitID = 'N/A'
-  }
-  else
-  {
-    Serial.print(F("UnitID = "));
-    Serial.println(F(UnitID));
   }
 }
