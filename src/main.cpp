bb1f391928205ad2f8c5e8cfa028a136968add3c
SamBozman <s.j.bozman@gmail.com>
2020-06-09 06:36:35 -0700

    OTHER additions

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@ void loop() {
   runSensorState = digitalRead(RUN_SENSOR);
   check_status();
   currentMQTT_Millis = millis();
-
+  // NOTE: WIFI on PC and DEVICE MUST BE THE SAME ###########################
   if (WiFi.status() == WL_CONNECTED) {
     if ((strcmp("N/A", UnitID) == 0) &&
         (currentMQTT_Millis - startMQTT_Millis >= MQTT_Interval)) {
